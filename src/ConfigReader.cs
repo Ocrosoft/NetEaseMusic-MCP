39d2a049e649db99d188a0000d61d3e68d8f1b08
ocrosoft <ocrosoft@ocrosoft.com>
2025-04-23 10:21:32 +0800

    feat: add configure reader for reading appsettings.json

---
+++ src/ConfigReader.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace NetEaseMusic_MCP
+{
+    internal class ConfigReader
+    {
+        private static IConfiguration? _configuration = null;
+        private static IConfiguration Configuration
+        {
+            get
+            {
+                if (_configuration != null)
+                {
+                    return _configuration;
+                }
+                ConfigurationBuilder builder = new();
+                builder.SetBasePath(Directory.GetCurrentDirectory());
+                builder.AddJsonFile("appsettings.json");
+                _configuration = builder.Build();
+                return _configuration;
+            }
+        }
+
+        public static T? GetConfig<T>(string key)
+        {
+            var config = Configuration[key];
+            if (config == null)
+            {
+                return default;
+            }
+            return (T)Convert.ChangeType(config, typeof(T));
+        }
+    }
+}
